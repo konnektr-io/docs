08822cf4376ae6cca5921a7577714de407589f44
Niko Raes <niko.raes@arcadis.com>
2025-09-27 15:42:32 +0200

    fix: standardize string quotes in source.config.ts
    chore: add launch configuration for debugging in VSCode

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,12 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
+// import { Mermaid } from "@/components/mdx/mermaid";
 import type { MDXComponents } from "mdx/types";
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...TabsComponents,
+    // Mermaid,
     ...components,
   };
 }
