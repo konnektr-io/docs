feadedbb8e02a8966a2888e02341e312722b2dbd
Niko Raes <niko.raes@gmail.com>
2025-12-18 13:00:32 +0100

    Remove deprecated components and files; add new blog functionality and update dependencies

--- source.config.ts
+++ source.config.ts
@@ -1,14 +1,18 @@
 import {
+  defineCollections,
   defineConfig,
   defineDocs,
   frontmatterSchema,
   metaSchema,
-} from 'fumadocs-mdx/config';
+} from "fumadocs-mdx/config";
+import jsonSchema from "fumadocs-mdx/plugins/json-schema";
+import lastModified from "fumadocs-mdx/plugins/last-modified";
+import { z } from "zod";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
   docs: {
     schema: frontmatterSchema,
     postprocess: {
@@ -20,7 +24,23 @@ export const docs = defineDocs({
   },
 });
 
+export const blog = defineCollections({
+  type: "doc",
+  dir: "content/blog",
+  schema: frontmatterSchema.extend({
+    author: z.string(),
+    date: z.iso.date().or(z.date()),
+  }),
+  async: true,
+});
+
 export default defineConfig({
+  plugins: [
+    jsonSchema({
+      insert: true,
+    }),
+    lastModified(),
+  ],
   mdxOptions: {
     // MDX options
   },
