60bd878a356afe49c741ac191e19c90471f20df5
Niko Raes <niko.raes@gmail.com>
2025-12-18 18:22:13 +0100

    Remove linkItems from DocsLayout and comment out unused GitHub link in layout.shared.tsx

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,11 +1,11 @@
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 // import { DocsLayout } from "fumadocs-ui/layouts/notebook";
-import { baseOptions, linkItems } from "@/lib/layout.shared";
+import { baseOptions } from "@/lib/layout.shared";
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()} links={linkItems}>
+    <DocsLayout tree={source.pageTree} {...baseOptions()}>
       {children}
     </DocsLayout>
   );
