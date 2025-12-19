ce60b72559d8a2b622fd20ddbb306f9461c2ce77
Niko Raes <niko.raes@gmail.com>
2025-12-19 09:22:53 +0100

    feat: Add initial documentation for DB Query Operator and configure redirects.

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,31 @@ const withMDX = createMDX();
 const config = {
   reactStrictMode: true,
   output: 'standalone',
-  experimental: { turbopackUseSystemTlsCerts: true }
+  experimental: { turbopackUseSystemTlsCerts: true },
+  async redirects () {
+    return [
+      {
+        source: '/docs/graph',
+        destination: '/docs/graph/introduction',
+        permanent: true,
+      },
+      {
+        source: '/docs/ktrlplane',
+        destination: '/docs/ktrlplane/introduction',
+        permanent: true,
+      },
+      {
+        source: '/docs/db-query-operator',
+        destination: '/docs/db-query-operator/introduction',
+        permanent: true,
+      },
+      {
+        source: '/docs/jexl',
+        destination: '/docs/jexl/introduction',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
